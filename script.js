3d9ef8bb56426cd7199abb345216a91667f96446
Pidgicle <s_thomo@hotmail.co.uk>
2022-08-23 21:04:19 +0100

    Added the display and started working on the enter function that will break down the question to a single number

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// MATHEMATICAL FUNCTIONS
+
 function add(a,b){
     return a + b;
 }
@@ -14,8 +16,12 @@ function divide(a,b){
     return a/b;
 }
 
-function operate(a,operator,b){
-    //operator.toString()
+// OPERATION FUNCTION TAKES THE SUM AND CALLS THE CORRECT FUNCTION
+
+function operate(x,operator,y){
+    console.log(`recieved ${x}, ${operator} and ${y}`)
+    let a = Number(x);
+    let b = Number(y);
     switch(operator){
         case '+':
             return add(a,b);
@@ -30,3 +36,40 @@ function operate(a,operator,b){
             return 'OOPS';
     }
 }
+
+const inputs = document.getElementsByClassName("input");
+const dispBot = document.querySelector('#botDisplay');
+const dispTop = document.querySelector('#topDisplay');
+const equals = document.querySelector('#equals')
+const clear = document.querySelector('#clear');
+
+Array.from(inputs).forEach(function(input){
+    input.addEventListener('click', function(){
+        dispBot.textContent += this.textContent;
+    })
+})
+
+clear.addEventListener('click', function(){
+    dispBot.textContent = '';
+    dispTop.textContent = '';
+})
+
+equals.addEventListener('click', function(){
+    let elements = dispBot.textContent.split(' ')
+    //while(elements.length > 1){
+        let opInd = 0;
+        if(elements.includes('x'||'÷')){
+            console.log("contains operator")
+            let opInd = elements.indexOf('x'||'÷')
+            elements[opInd] = operate(elements[opInd-1],elements[opInd], elements[opInd+1])
+            elements.splice(opInd-1,1);
+            elements.splice(opInd,1);
+      //      continue;
+        }
+    //}
+    //console.log(elements)
+
+    dispTop.textContent = dispBot.textContent;
+    dispBot.textContent = elements[0]
+    
+})
